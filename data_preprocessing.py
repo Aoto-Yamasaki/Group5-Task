d47dae03529e857aeab6c599bde5856387182be5
Aoto-Yamasaki <aoto2002@gmail.com>
2025-07-17 11:08:17 +0900

    Plot SVM results

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+def main():
+  
+    preprocess(pd.read_csv(f"./metadata/{filenames[0]}"))
+
 filenames = ["LegalEagle_metadata.csv", 
              "Mark Rober_metadata.csv", 
              "Matt D'Avella_metadata.csv", 
@@ -12,4 +16,12 @@ filenames = ["LegalEagle_metadata.csv",
              "Yes Theory_metadata.csv"
              ]
 
-pd.read_csv(filename)
+# for filename in filenames:
+#     pd.read_csv(filename)
+
+def preprocess(data: pd.DataFrame):
+    answer = data.head(20)
+    answer.append(data.tail(20), ignore_index = True, inplace = True)
+
+if __name__ == "__main__":
+    main()  
